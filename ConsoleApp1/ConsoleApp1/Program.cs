2d5e2c03a315ca8c31fbcc0ac98eb2d4e4df73c0
larry Aguilar <larryaguilar00007@gmail.com>
2025-08-31 17:38:53 +0800

    v1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to C# Sample Program.");
+
+            // Ask for user's name
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            // Ask for user's age
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            // Process and display output
+            Console.WriteLine($"\nNice to meet you, {name}!");
+            Console.WriteLine($"Next year, you will be {age + 1} years old.");
+
+            // Keep console open
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
