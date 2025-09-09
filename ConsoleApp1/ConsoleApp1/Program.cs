63d5921853fadf63d3cf281e671201d69212c663
larry Aguilar <larryaguilar00007@gmail.com>
2025-09-09 22:37:05 +0800

    HW1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string action;
+            int x, y;
             Console.WriteLine("Hello! Welcome to C# Sample Program.");
 
             // Ask for user's name
@@ -22,7 +24,34 @@ namespace ConsoleApp1
 
             // Process and display output
             Console.WriteLine($"\nNice to meet you, {name}!");
-            Console.WriteLine($"Next year, you will be {age + 1} years old.");
+            Console.WriteLine($"Next year, you will be {age + 1} years old. \n");
+
+            Console.Write("Enter a number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a second number: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Operator : ");
+            action = Console.ReadLine();
+
+            if (action == "+")
+            {
+                Console.WriteLine($"the sum off " + x + " and " + y + " is = " + (x + y));
+            }
+            else if (action == "-")
+            {
+                Console.WriteLine($"the subtract off " + x + " and " + y + " is = " + (x - y));
+            }
+            else if (action == "/") 
+            {
+                Console.WriteLine($"the divition off " + x + " and " + y + " is = " + (x / y));
+            }
+            else if (action == "x" || action == "*")
+            {
+                Console.WriteLine($"the multiplication off " + x + " and " + y + " is = " + (x * y));
+            }
+
 
             // Keep console open
             Console.WriteLine("\nPress any key to exit...");
---
+++ activity/activity/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int x = 1; x <= 5; x++)
+            {
+                for(int i= 6; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine(x);
+            }
+            //int j;
+           // Console.Write("Enter a size: ");
+           // j = int.Parse(Console.ReadLine());
+
+            //int a;
+
+           // while (true)
+            //{
+           //     Console.Write("Enter number: ");
+            //    a = int.Parse(Console.ReadLine());
+             //   Console.WriteLine(a);
+               
+                
+           // }
+           // string str, str_output;
+           /* Console.Write("Enter a str: ");
+            str = Console.ReadLine();
+            for (int x = 0; x < str.Length; x++)
+            {
+                str_output = str[x].ToString() == "a" ? "Vowel" ;
+                str[x].ToString() == "e" ? "Vowel" ;
+                str[x].ToString() == "i" ? "Vowel" ;
+                str[x].ToString() == "o" ? "Vowel" ;
+                str[x].ToString() == "u" ? "Vowel" ;
+                Console.WriteLine($"{str[x]} {str_output}");
+
+            }
+           */
+            int row, column, search;
+
+            Console.Write("Enter a Row size: ");
+            row = int.Parse(Console.ReadLine());
+            Console.Write("Enter a Column: ");
+            column = int.Parse(Console.ReadLine());
+            Console.Write("Search: ");
+            search = int.Parse(Console.ReadLine());
+
+            if(row == column)
+            {
+                Console.Write(" ".Length +(row * column));
+            }
+             
+            Console.ReadKey();
+        }    
+    }
+}
